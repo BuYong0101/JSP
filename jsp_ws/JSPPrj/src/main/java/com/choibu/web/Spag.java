bac2f92c5982982fd779b4efab63b9ccf3f4ae4d
ddonggirim <choibu02222@naver.com>
2024-12-04 17:35:12 +0900

    jsp홈페이지

---
+++ jsp_ws/JSPPrj/src/main/java/com/choibu/web/Spag.java
@@ -0,0 +1,49 @@
+package com.choibu.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/spag")
+public class Spag extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int num = 0;
+		String num_ = request.getParameter("n");
+		if(num_ != null && !num_.equals(""))
+			num = Integer.parseInt(num_);
+		
+		
+		String result;
+		
+		if(num%2 != 0)
+			result = "홀수";
+		else
+			result = "짝수";
+		
+		request.setAttribute("result", result);
+		
+		String[] names = {"choibu", "dragon"};
+		request.setAttribute("names", names);
+		
+		Map<String, Object> notice = new HashMap<String, Object>();
+		notice.put("id", 1);
+		notice.put("title", "EL은 좋아요");
+		request.setAttribute("notice", notice);
+		
+		//redirect - 새로운 실행
+		//forward - 이어서 실행
+		RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("spag.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+}
