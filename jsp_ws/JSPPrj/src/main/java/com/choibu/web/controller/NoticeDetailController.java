6f01860f4572935e7be439758ad4d9f1936c3cd9
ddonggirim <choibu02222@naver.com>
2024-12-09 21:22:05 +0900

    jstl

--- jsp_ws/JSPPrj/src/main/java/com/choibu/web/controller/NoticeDetailController.java
+++ jsp_ws/JSPPrj/src/main/java/com/choibu/web/controller/NoticeDetailController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.choibu.web.entity.Notice;
+import com.choibu.web.service.NoticeService;
 
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet{
@@ -22,58 +23,10 @@ public class NoticeDetailController extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		int id = Integer.parseInt(request.getParameter("id"));
-		
-		String url = "jdbc:oracle:thin:@localhost:1521/xe";
-		String sql = "SELECT * FROM NOTICE WHERE ID=?";
 	
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "scott", "tiger");
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setInt(1, id);
-			
-			ResultSet rs = st.executeQuery();
-			
-			rs.next();
-			
-			String title = rs.getString("TITLE");
-			String writerId = rs.getString("WRITER_ID");
-			Date regdate = rs.getDate("REGDATE");
-			String hit = rs.getString("HIT");
-			String files = rs.getString("FILES");
-			String content = rs.getString("CONTENT"); 
-			
-			Notice notice = new Notice(
-						id,
-						title,
-						writerId,
-						regdate,
-						hit,
-						files,
-						content
-					);
-			
-			request.setAttribute("n", notice);
-			
-			/* 
-			request.setAttribute("title", title);
-			request.setAttribute("writerId", title);
-			request.setAttribute("regdate", title);
-			request.setAttribute("hit", title);
-			request.setAttribute("files", title);		
-			request.setAttribute("content", content);
-			*/
-			rs.close();
-			st.close();
-			con.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		NoticeService service = new NoticeService();
+		Notice notice = service.getNotice(id);
+		request.setAttribute("n", notice);
 		
 		request.getRequestDispatcher("/WEB-INF/view/notice/detail.jsp")
 			   .forward(request, response);
--- jsp_ws/JSPPrj/src/main/java/com/choibu/web/controller/NoticeListController.java
+++ jsp_ws/JSPPrj/src/main/java/com/choibu/web/controller/NoticeListController.java
@@ -17,55 +17,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.choibu.web.entity.Notice;
+import com.choibu.web.entity.NoticeView;
+import com.choibu.web.service.NoticeService;
 
 @WebServlet("/notice/list")
 public class NoticeListController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		List<Notice> list = new ArrayList<>();
-		
-		String url = "jdbc:oracle:thin:@localhost:1521/xe";
-		String sql = "SELECT * FROM NOTICE";
-
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "scott", "tiger");
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-
-			while(rs.next()){	
-				int id = rs.getInt("ID");
-				String title = rs.getString("TITLE");
-				String writerId = rs.getString("WRITER_ID");
-				Date regdate = rs.getDate("REGDATE");
-				String hit = rs.getString("HIT");
-				String files = rs.getString("FILES");
-				String content = rs.getString("CONTENT"); 
 				
-				Notice notice = new Notice(
-						id,
-						title,
-						writerId,
-						regdate,
-						hit,
-						files,
-						content
-				);
-				list.add(notice);
-			}
-				
-			   rs.close();
-			   st.close();
-			   con.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String field_ = request.getParameter("f");
+		String query_ = request.getParameter("q");
+		String page_ = request.getParameter("p");
+		
+		String field = "title";
+		if(field_ != null && !field_.equals("")) 
+			field = field_;
+		
+		String query = "";
+		if(query_ != null && !query_.equals("")) 
+			query = query_;
+		
+		int page = 1;
+		if(page_ != null && !page_.equals("")) 
+			page = Integer.parseInt(page_);
+		
+		NoticeService service = new NoticeService();
+		List<NoticeView> list = service.getNoticeList(field, query, page);
+		int count = service.getNoticeCount(field, query);
+	
 		request.setAttribute("list", list);
+		request.setAttribute("count", count);;
+		
 		
 		request.getRequestDispatcher("/WEB-INF/view/notice/list.jsp")
 		   .forward(request, response);
